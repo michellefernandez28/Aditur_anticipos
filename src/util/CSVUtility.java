6813ab9a10b5e44b9b2548f852a699dddf929a43
Francisco-ZC <franzamo14.fzc@gmail.com>
2026-03-08 13:39:00 -0600

    Structural and DAO changes
    "ingles" was changed to "model"
    "util" and "dao" folders were added.
    ClienteXEventoDAO was created

---
+++ src/util/CSVUtility.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ *
+ * @author Usuario
+ */
+public class CSVUtility {
+
+    public static List<String> leerLineas(String archivo) throws IOException {
+
+        List<String> lineas = new ArrayList<>();
+
+        File file = new File(archivo);
+
+        if (!file.exists()) {
+            return lineas;
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(archivo))) {
+
+            String linea;
+
+            while ((linea = br.readLine()) != null) {
+                lineas.add(linea);
+            }
+        }
+
+        return lineas;
+    }
+
+    public static void escribirLinea(String archivo, String linea) throws IOException {
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(archivo, true))) {
+
+            bw.write(linea);
+            bw.newLine();
+
+        }
+
+    }
+
+    public static void asegurarArchivo(String ruta, String header) throws IOException {
+
+        File archivo = new File(ruta);
+
+        // crear carpeta si no existe
+        File parent = archivo.getParentFile();
+        if (parent != null) {
+            parent.mkdirs();
+        }
+
+        if (!archivo.exists()) {
+
+            BufferedWriter bw = new BufferedWriter(new FileWriter(archivo));
+            bw.write(header);
+            bw.newLine();
+            bw.close();
+        }
+    }
+
+}
---
+++ src/util/Constantes.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Constantes {
+
+    public static final String SEP = ";";
+
+    public static final String CARPETA_DATOS = "data";
+    public static final String ARCHIVO_CLIENTES = CARPETA_DATOS + "/clientes.csv";
+    public static final String ARCHIVO_EVENTOS = CARPETA_DATOS + "/eventos.csv";
+    public static final String ARCHIVO_PAGOS = CARPETA_DATOS + "/pagos.csv";
+    public static final String ARCHIVO_CLIENTE_EVENTO = CARPETA_DATOS + "/cliente_evento.csv";
+}
